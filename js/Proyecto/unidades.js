3d9d732e6db9f382d194c7dd16fb66fad64baad0
Matias Souto <masouto94@gmail.com>
2021-07-10 14:48:34 -0300

    Commit antes de hacer las formulas nuevas de verdad

--- js/Proyecto/unidades.js
+++ js/Proyecto/unidades.js
@@ -1,5 +1,2 @@
 var Jaguar_Warrior = new Melee_Unit("Jaguar Warrior", 50, {"melee": 10}, {"Infantry": 10, "Condottiero": 10, "Eagle Warrior": 2, "Standard Building": 2}, 2.0, 0, {"base_melee":1, "base_pierce": 1, "Infantry": 0, "Unique Unit": 0}, {"food":60, "wood":0, "gold":30}, 3, 1);
-
-// var Long_Swordsman = new Melee_Unit(name_unit_2,hp_unit_2,attack_unit_2,bonus_unit_2,ROF_unit_2,blast_radius_unit_2,armorclasses_unit_2,cost_unit_2,LOS_unit_2,speed_unit_2);
-
-console.log(Jaguar_Warrior)
+var Long_Swordsman = new Melee_Unit("Long Swordsman", 60, {"melee": 9}, {"Eagle Warrior": 6, "Standard Building": 3}, 2, 0, {"base_melee":0, "base_pierce":1, "Infantry":0}, {"food": 60, "wood":0, "gold": 20}, 4, 0.9);
